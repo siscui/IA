ab8f22677bf79e6ed1f79adbfaf152f398f03fce
Alvaro Arando Arce <alvaro.arando@gmail.com>
2019-10-05 17:31:50 -0300

    separar en funciones

--- main.py
+++ main.py
@@ -4,42 +4,47 @@
 import subprocess
 import sys
 
-image_path = sys.argv[1]
 
-detectarEspecie = 'python label_image.py \
-                    --graph=modelos/especies_graph.pb \
-                    --labels=modelos/especies_labels.txt \
-                    --input_layer=Placeholder \
-                    --output_layer=final_result \
-                    --image=' + image_path
-
-print("Detectando especie...")
-
-
-def getresult(input_process):
+def getresult(model):
+    input_process = subprocess.Popen(model, stdout=subprocess.PIPE, stderr=None, shell=True)
     output = input_process.communicate()[0]
     output = output.decode()
     result = output.split('\n')
     return result[0]
 
 
-process_detection = subprocess.Popen(detectarEspecie, stdout=subprocess.PIPE, stderr=None, shell=True)
+def detect_specie(image_path):
+    detectarEspecie = 'python label_image.py \
+                    --graph=modelos/especies_graph.pb \
+                    --labels=modelos/especies_labels.txt \
+                    --input_layer=Placeholder \
+                    --output_layer=final_result \
+                    --image=' + image_path
+    detection = getresult(detectarEspecie)
+    detected_species = detection.split(' ')[0]
+    return detected_species
 
-detection = getresult(process_detection)
 
-detected_species = detection.split(' ')[0]
-print("especie detectada " + detection)
-
-if detected_species == 'tomate' or detected_species == 'morron':
-    print("Analizando madurez...")
-    detectarMadurez = 'python label_image.py \
+def detected_mature(detected_species):
+    if detected_species == 'tomate' or detected_species == 'morron':
+        print("Analizando madurez...")
+        detectarMadurez = 'python label_image.py \
                     --graph=modelos/madurez_' + detected_species + '_graph.pb \
                     --labels=modelos/madurez_' + detected_species + '_labels.txt \
                     --input_layer=Placeholder \
                     --output_layer=final_result \
                     --image=' + image_path
 
-    process_mature = subprocess.Popen(detectarMadurez, stdout=subprocess.PIPE, stderr=None, shell=True)
+        mature = getresult(detectarMadurez)
+        detected_matures = mature.split(' ')[0]
+        return detected_matures
 
-    mature = getresult(process_mature)
-    print(mature)
+
+image_path = sys.argv[1]
+print("Detectando especie...")
+
+result_detection = detect_specie(image_path)
+print("especie detectada " + result_detection)
+
+result_mature = detected_mature(result_detection)
+print(result_mature)
